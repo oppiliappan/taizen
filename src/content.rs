938423224b0cfed3d7a36494bdf56b62e186b1c1
NerdyPepper <akshayoppiliappan@gmail.com>
2018-06-17 08:25:43 +0530

    Refactor into content.rs

---
+++ src/content.rs
@@ -0,0 +1,38 @@
+pub fn url_gen(title: &str) -> String {
+
+//  /w/api.php?action=query&format=json&prop=extracts&titles=rust&explaintext=1
+    title.replace(" ", "%20");
+
+    // query config
+    let url = "https://en.wikipedia.org";
+    url.push_str("w/api.php?");
+    url.push_str("action=query&");
+    url.push_str("format=json&");
+    url.push_str("prop=extracts&");
+    url.push_str(format!("titles={}", title));
+    url.push_str("explaintext=1");
+}
+
+pub fn get_extract(title: &str, red: Response) -> String {
+    let mut v: Value = serde_json::from_str(&res.text().unwrap()).unwrap();
+
+    // Fetch page and pageids of requested title(s)
+    let pageid = &v["query"]["pageids"][0];
+    let pageid_str = match pageid {
+        Value::String(id) => id,
+        _ => panic!("wut"),
+    };
+
+    if pageid_str == "-1" {
+        String::from("No such page")
+    } else {
+        format!("{}", &v["query"]["pages"][pageid_str]["extract"])
+    }
+}
+
+pub fn get_title(title: &str, res: Response) -> String {
+    let mut v: Value = serde_json::from_str(&res.text().unwrap()).unwrap_or_else( |e| {
+        panic!("Recieved invalid json");
+    } );
+    format!("{}", &v["query"]["normalized"][0]["to"])
+}
--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,21 @@ extern crate serde_json;
 
 use serde_json::Value;
 
-fn main() {
-    println!("{}", get_extract("scale"));
-}
+pub mod content;
 
-fn get_extract(title: &str) -> String {
+fn main() {
     let url = format!("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&list=&meta=&indexpageids=1&continue=%7C%7Cimageinfo&titles={}&exlimit=20&explaintext=1&exsectionformat=plain", title);
     let res = reqwest::get(&url);
 
     match res {
-        Ok(mut res) => {
+        Ok(res) => {
             if res.status().is_success() {
-                let mut v: Value = serde_json::from_str(&res.text().unwrap()).unwrap();
-
-                // Fetch page and pageids of requested title(s)
-                let pageid = &v["query"]["pageids"][0];
-                let pageid_str = match pageid {
-                    Value::String(id) => id,
-                    _ => panic!("wut"),
-                };
-
-                format!("{:#}", &v["query"]["pages"][pageid_str]["extract"])
-            } else {
-                format!("Error while parsing url.\nRecieved {}", res.status())
+                content::get_extract("")
             }
-        },
-        Err(_) => {
-            format!("Failed to parse URL")
+        }
+
+        Err(_) {
+            panic!("Oh no!");
         }
     }
 }
